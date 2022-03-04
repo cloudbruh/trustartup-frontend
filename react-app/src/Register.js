60b752ae0ac79e6b1133b2c761341284ba531073
Kirill <shabad.k@yandex.ru>
2022-03-04 13:07:19 +0300

    Регистрация пользователя и сохранение токена

--- react-app/src/Register.js
+++ react-app/src/Register.js
@@ -1,34 +1,86 @@
 import React from 'react';
 import Card from './Card'
 import Filters from './Filters'
+import './cookie'
 
 class Register extends React.Component {
 
-    state = {
-        title: 'Регистрациия'
-    }
+    constructor(props) {
+        super(props);
+        this.state = {name: '',
+                      surname: '',
+                      email: '',
+                      pass: ''};
+        this.handleChangeName = this.handleChangeName.bind(this);
+        this.handleChangeSurname = this.handleChangeSurname.bind(this);
+        this.handleChangeEmail = this.handleChangeEmail.bind(this);
+        this.handleChangePass = this.handleChangePass.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+
+    async handleSubmit(event) {
+        let req;
+        try{
+        req = await fetch('http://192.168.1.69:8087/register?name=' + this.state.name + '&surname=' + this.state.surname + '&email=' + this.state.email + '&password=' + this.state.pass,
+        {method: 'POST',
+        headers: {  
+          "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"  
+        },  
+         mode: 'cors'})
+      }
+      catch(e)
+      {
+        alert(e)
+      }
+        let resp = await  req.json()
+        if(!req.ok)
+          alert(resp.message)
+        else
+        {
+          window.cookie.set('token', resp.token)  
+        }
+        event.preventDefault();
+      }
+
+      handleChangeName(event) {
+        this.setState({name: event.target.value});
+      }
+
+      handleChangeSurname(event) {
+        this.setState({surname: event.target.value});
+      }
+
+      handleChangeEmail(event) {
+        this.setState({email: event.target.value});
+      }
+
+      handleChangePass(event) {
+        this.setState({pass: event.target.value});
+      }
     
     render() {
         return (
-            <div className='mx-auto w-fit mt-10 p-5 flex flex-col justify-center'>
-                <div className='user-name mb-7'>
-                        <label htmlFor='user-name' className='font-bold mr-2 mb-2 block'>ФИО:</label>
-                        <input type='text' id='user-name' className='border border-solid rounded-sm'/>
+            <form onSubmit={this.handleSubmit}>
+                <div className='mx-auto w-fit mt-10 p-5 flex flex-col justify-center'>
+                    <div className='user-name mb-7'>
+                            <label htmlFor='user-name' className='font-bold mr-2 mb-2 block'>Имя:</label>
+                            <input type='text' onChange={this.handleChangeName} value={this.state.name} id='user-name' className='border border-solid rounded-sm'/>
+                    </div>
+                    <div className='user-tel mb-7'>
+                            <label htmlFor='user-surname' className='font-bold mr-2 mb-2 block'>Фамилия:</label>
+                            <input type='text' onChange={this.handleChangeSurname} value={this.state.surname} id='user-surname' className='border border-solid rounded-sm'/>
+                    </div>
+                    <div className='user-email mb-7'>
+                            <label htmlFor='user-email' className='font-bold mr-2 mb-2 block'>E-mail:</label>
+                            <input type='text' onChange={this.handleChangeEmail} value={this.state.email} id='user-email' className='border border-solid rounded-sm'/>
+                    </div>
+                    <div className='user-pass mb-7'>
+                            <label htmlFor='user-pass' className='font-bold mr-2 mb-2 block'>Пароль:</label>
+                            <input type='password' onChange={this.handleChangePass} value={this.state.pass} id='user-pass' className='border border-solid rounded-sm'/>
+                    </div>
+                    <input type='button' onClick={this.handleSubmit} value='Сохранить' className='register-save py-2 px-4 text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700'/>
                 </div>
-                <div className='user-tel mb-7'>
-                        <label htmlFor='user-tel' className='font-bold mr-2 mb-2 block'>Телефон:</label>
-                        <input type='text' id='user-tel' className='border border-solid rounded-sm'/>
-                </div>
-                <div className='user-email mb-7'>
-                        <label htmlFor='user-email' className='font-bold mr-2 mb-2 block'>E-mail:</label>
-                        <input type='text' id='user-email' className='border border-solid rounded-sm'/>
-                </div>
-                <div className='user-pass mb-7'>
-                        <label htmlFor='user-pass' className='font-bold mr-2 mb-2 block'>Пароль:</label>
-                        <input type='password' id='user-pass' className='border border-solid rounded-sm'/>
-                </div>
-                <button className='register-save py-2 px-4 text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700'>Сохранить</button>
-            </div>
+            </form>
         )
     }
 }
---
+++ react-app/src/cookie.js
@@ -0,0 +1,59 @@
+/**
+ * Работа с куками
+ */
+(function (w) {
+    var Cookie = function () {
+        if (!(this instanceof Cookie)) return new Cookie();
+    };
+
+    var p = Cookie.prototype;
+
+    p.set = function (name, value, options) {
+        var opt = {
+            path: "/",
+            "max-age": 604800,
+            // при необходимости добавьте другие значения по умолчанию
+        };
+
+        if (options && typeof options === "object")
+            for (var key in options) opt[key] = options[key];
+        // else return false;
+
+        if (opt.expires instanceof Date) {
+            opt.expires = opt.expires.toUTCString();
+        }
+
+        var updatedCookie =
+            encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+        for (var optionKey in opt) {
+            updatedCookie += "; " + optionKey;
+            var optionValue = opt[optionKey];
+            if (optionValue !== true) {
+                updatedCookie += "=" + optionValue;
+            }
+        }
+
+        document.cookie = updatedCookie;
+    };
+
+    p.get = function (name) {
+        var matches = document.cookie.match(
+            new RegExp(
+                "(?:^|; )" +
+                    name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") +
+                    "=([^;]*)"
+            )
+        );
+        return matches ? decodeURIComponent(matches[1]) : undefined;
+    };
+
+    p.remove = function (name) {
+        this.set(name, "", {
+            "max-age": -1,
+        });
+    };
+
+    // Добавляем прототип в глобальную переменную
+    w.Cookie = Cookie;
+})(window);
