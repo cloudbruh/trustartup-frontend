2e850acb95aeab0b028308a39f6b9d337d2fb0e2
Kirill <shabad.k@yandex.ru>
2022-03-06 05:11:22 +0300

    Карточки для страницы модератора

--- react-app/src/ModeratorPage.js
+++ react-app/src/ModeratorPage.js
@@ -2,21 +2,21 @@ import React from 'react';
 
 class ModeratorPage extends React.Component {
   state = {
-    pendingStartups: [],
-    pendingUsers: [],
+    pendingStartups: [{id: 0, name: 'SampleStartupName'}],
+    pendingUsers: [{id: 0, name: 'SampleUserName'}],
   };
 
   render() {
     return (
       <div>
-        <h1>Стартапы, требующие подтверждения</h1>
+        <h1 className='text-center font-bold text-xl my-5'>Стартапы, требующие подтверждения</h1>
         {this.state.pendingStartups.map(startup => {
           return (
             <ModeratorStartupCard startup={startup} />
           );
         })}
 
-        <h1>Пользователи, требующие подтверждения</h1>
+        <h1 className='text-center font-bold text-xl my-5'>Пользователи, требующие подтверждения</h1>
         {this.state.pendingUsers.map(user => {
           return (
             <ModeratorUserCard user={user} />
@@ -30,9 +30,18 @@ class ModeratorPage extends React.Component {
 class ModeratorStartupCard extends React.Component {
   render() {
     return (
-      <div>
-        <p>Id: {this.props.startup.id}</p>
-        <p>Name: {this.props.startup.name}</p>
+      <div className='card bg-card border-solid mx-auto relative w-80 h-80 px-2 py-5 mb-20'>
+        <div className='text-center'>
+          <p>Id: {this.props.startup.id}</p>
+          <p>Name: {this.props.startup.name}</p>
+        </div>
+        <div className='img'>
+          <img className='mx-auto mt-5' width={200} height={400} src='images/batman.svg' alt='документ компании'/>
+        </div>
+        <div className='text-center'>
+          <button className='mt-10 mb-5 py-2 px-4 text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700'>Одобрить</button>
+          <button className='mt-10 ml-5 mb-5 py-2 px-4 text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700'>Отклонить</button>
+        </div>
       </div>
     );
   }
@@ -41,9 +50,18 @@ class ModeratorStartupCard extends React.Component {
 class ModeratorUserCard extends React.Component {
   render() {
     return (
-      <div>
-        <p>Id: {this.props.user.id}</p>
-        <p>Name: {this.props.user.name}</p>
+      <div className='card bg-card border-solid mx-auto relative w-80 h-80 px-2 py-5 mb-20'>
+        <div className='text-center'>
+          <p>Id: {this.props.user.id}</p>
+          <p>Name: {this.props.user.name}</p>
+        </div>
+        <div className='img'>
+          <img className='mx-auto mt-5' width={200} height={400} src='images/batman.svg' alt='документ компании'/>
+        </div>
+        <div className='text-center'>
+          <button className='mt-10 mb-5 py-2 px-4 text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700'>Одобрить</button>
+          <button className='mt-10 ml-5 mb-5 py-2 px-4 text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700'>Отклонить</button>
+        </div>
       </div>
     );
   }
