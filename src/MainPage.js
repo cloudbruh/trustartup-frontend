594a9f29e49029b7073c00657f4b7d1599a5b3a3
MilkyCode13 <shmayhel.andrey@gmail.com>
2022-03-11 21:43:36 +0300

    Added basic pagination

--- src/MainPage.js
+++ src/MainPage.js
@@ -9,6 +9,9 @@ class MainPage extends React.Component {
 
     state = {
         startups: [],
+        canLoadMore: true,
+        offset: 0,
+        isLoading: false,
     };
 
     constructor(props)
@@ -16,32 +19,53 @@ class MainPage extends React.Component {
         super(props)
         props.onTitleChanged('Лента')
         this.state.token = window.cookie.get('token')
+
+        this.loadData = this.loadData.bind(this);
     }
 
     componentDidMount() {
-        fetch('/api/feed/api/startupfeed', {
+        this.loadData();
+    }
+
+    loadData() {
+        if (this.state.isLoading) {
+            return;
+        }
+
+        this.setState({
+            isLoading: true
+        });
+        fetch('/api/feed/api/startupfeed?offset=' + this.state.offset, {
             headers: {
                 'Authorization': `Bearer ${this.state.token}`
             },
         })
-          .then(res => res.json())
-          .then((result) => {
+        .then(res => res.json())
+        .then((result) => {
             this.setState({
-                startups: result
+                startups: this.state.startups.concat(result),
+                canLoadMore: result.length > 0,
+                offset: this.state.offset + result.length,
+                isLoading: false,
             });
-          });
-      }
+        });
+    }
 
     render() {
         return (
             <div className='content mt-10 px-4 mx-auto'>
-                    <ul>
-                        {this.state.startups.map(startup => {
-                            return (
-                                <li key={startup.id}><StartupCard startup={startup} token={this.state.token} /></li>
-                            );
-                        })}
-                    </ul>
+                <ul>
+                    {this.state.startups.map(startup => {
+                        return (
+                            <li key={startup.id}><StartupCard startup={startup} token={this.state.token} /></li>
+                        );
+                    })}
+                </ul>
+                {(this.state.isLoading && (
+                    <div className='mx-auto my-5 text-xl text-center'>Загрузка</div>
+                )) || (this.state.canLoadMore && (
+                    <button onClick={this.loadData} className='mx-auto my-5 p-2 block rounded-xl text-xl text-center bg-gray-200 hover:bg-blue hover:text-white'>Загрузить больше</button>
+                ))}
             </div>
         )
     }
