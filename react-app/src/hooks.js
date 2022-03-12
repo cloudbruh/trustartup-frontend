9c6ee3b74b840516e57b1d93afbf21eddbb671b4
Kirill <34163701+rorShaK03@users.noreply.github.com>
2022-03-12 05:35:58 +0300

    Delete react-app directory

--- react-app/src/hooks.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { useParams } from "react-router-dom";
-
-function withParams(Component) {
-  return props => <Component {...props} params={useParams()} />;
-}
-
-export default withParams;
