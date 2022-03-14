379ee99096db7e638c5d54cd977f02dbd89fa58a
Kirill <shabad.k@yandex.ru>
2022-03-14 19:06:24 +0300

    Отображение заявок в ЛК

---
+++ src/components/Application.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import * as config from '../helpers/config';
+
+class Application extends React.Component {
+
+    types = {
+        'ROLE_CREATOR': 'Получение роли стартапера',
+        'ROLE_APPLICANT': 'Получение роли  соискателя',
+        'ROLE_MODERATOR': 'Получение роли модератора',
+        'STARTUP': 'Создание стартапа'
+    }
+
+    statuses = {
+        'GRANTED': 'Одобрено',
+        'PROHIBITED': 'Отклонено'
+    }
+
+    render() {
+        return (
+            <div className="text-center mb-5 bg-card w-80 mx-auto">
+                <div>Тип заявки: {this.types[this.props.appl.moderatable_type]}</div>
+                <div>Статус: {this.statuses[this.props.appl.status]}</div>
+                <div>Мой комментарий: {this.props.appl.content}</div>
+                <div>Комментарий модератора: {this.props.appl.comment}</div>
+            </div>
+        )
+    }
+}
+
+export default Application;
